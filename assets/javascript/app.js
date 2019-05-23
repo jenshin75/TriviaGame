eea27ec93d6f09667a542d19c68cc9f31f5cffab
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-05-23 18:11:45 -0500

    updated 05/23/19 6:11pm

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,21 @@ $("#start").on("click", function(){
 var questions = [
   {
     question: "Q1: What is the longest river in the United States?",
-    options: ["Colorado River", "Missouri River", "Rio Grande River"],
-    correctAnswer: 2
+    options: ["Colorado River", "Missouri River", "Rio Grande River", "Lake Michigan"],
+    correctAnswer: 2,
+    url: "https://media.giphy.com/media/2jQtHNHA8woyp27Lzg/giphy.gif"
   },
   {
     question: "Q2: Death Valley is located in what U.S. state?",
     options: ["Nevada", "Texas", "California"],
-    correctAnswer: 3
+    correctAnswer: 3,
+    url: "https://giphy.com/gifs/61S9lWAlkZg4837zOu"
   },
   {
     question: "Q3: Pierre is the capital of what U.S. state?",
     options: ["South Dakota", "Montana", "Rhode Island"],
-    correctAnswer: 1
+    correctAnswer: 1,
+    url: "https://giphy.com/gifs/20NhnmyD3QYMrfnTgr"
   }
 ];
 
@@ -27,66 +30,82 @@ var incorrect = 0;
 var unanswered = 0;
 var timer;
 var counter = 99;
+var index = 0;
 
 function showQuestions() {
+var question = $("<p>");
 
-  //*show trivia questions and append them
-  for (let i = 0; i < questions.length; i++) {
-    $("#trivia").append("<h2>" + questions[i].question + "</h2>")
+question.append(questions[index].question + "<br>")
+question.append(questions[index].options[index])
 
-    //*show answer choices and append them using radio buttons
-    for (let index = 0; index < questions[i].options.length; index++) {
-      $("#trivia").append('<input type="radio" name="' + i + '"  res="' + index + '">' + questions[i].options[index])
-    }
-  }
-  //*dynamically create/append submit button
-  $("#trivia").append('<br><br><button id="submit">Submit</button>')
-  timer = setInterval(countdown, 1000)   // if we do not have a variable, we cannot clear the interval
+var urlImage = $("<img>") ;
+urlImage.attr("src", questions[index].url)
 
-  //when submit button is clicked, check "input" selected
-  $("#submit").on("click", function () {
-    var alldata = $("#trivia").children("input:checked")
-    clearInterval(timer);
-    // stop the timer
+question.append(urlImage);
+  $("#trivia").append(question)
+  
 
-    //store user's choices for each question in "alldata"
-    for (i = 0; i < alldata.length; i++) {
-      var q = $(alldata[i]).attr("name")
-      var r = $(alldata[i]).attr("res")
-      //split/parse user's question/correct answer to correct question/answer
-      if (parseInt(questions[q].correctAnswer) === parseInt(r)) {
-        correct++
-      }
-      else if (parseInt(questions[q].correctAnswer) !== parseInt(r)) {
-        incorrect++
-      }
-      else {
-        unanswered++
-      }
 
-      console.log("correct: ", correct);
-      console.log("incorrect: ", incorrect);
-      console.log("unanswered: ", unanswered);
-    }
 
-    //count down time left
-    function countdown() {
-      counter--;
-      $("#time-left").text("Time Remaining: " + counter)
+//   //*show trivia questions and append them
+//   for (let i = 0; i < questions.length; i++) {
+//     $("#trivia").append("<h2>" + questions[i].question + "</h2>")
 
-      if (counter === 0) {
-        clearInterval(timer)
-        alert("time out")
-      }
-    }
+//     //*show answer choices and append them using radio buttons
+//     for (let index = 0; index < questions[i].options.length; index++) {
+//       $("#trivia").append('<input type="radio" name="' + i + '"  res="' + index + '">' + questions[i].options[index])
+//     }
+//   }
+//   //*dynamically create/append submit button
+//   $("#trivia").append('<br><br><button id="submit">Submit</button>')
+//   timer = setInterval(countdown, 1000)   // if we do not have a variable, we cannot clear the interval
 
-    function reset() {
-      //reset your counter display here
-      //reset your counter to 0
-    }
+//   //when submit button is clicked, check "input" selected
+//   $("#submit").on("click", function () {
+//     var alldata = $("#trivia").children("input:checked")
+//     clearInterval(timer);
+//     // stop the timer
 
-    reset()
-    showQuestions()
-  })
+//     //store user's choices for each question in "alldata"
+//     for (i = 0; i < alldata.length; i++) {
+//       var q = $(alldata[i]).attr("name")
+//       var r = $(alldata[i]).attr("res")
+//       //split/parse user's question/correct answer to correct question/answer
+//       if (parseInt(questions[q].correctAnswer) === parseInt(r)) {
+//         correct++
+//       }
+//       else if (parseInt(questions[q].correctAnswer) !== parseInt(r)) {
+//         incorrect++
+//       }
+//       else {
+//         unanswered++
+//       }
+
+//       console.log("correct: ", correct);
+//       console.log("incorrect: ", incorrect);
+//       console.log("unanswered: ", unanswered);
+//     }
+
+//     //count down time left
+//     function countdown() {
+//       counter--;
+//       $("#time-left").text("Time Remaining: " + counter)
+
+//       if (counter === 0) {
+//         clearInterval(timer)
+//         alert("time out")
+//       }
+//     }
+
+//     function reset() {
+//       //reset your counter display here
+//       //reset your counter to 0
+//     }
+
+//     reset()
+//     showQuestions()
+//   })
+
 }
 
+showQuestions()
