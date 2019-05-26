be3aab3e08bea37f27a6e4f8316db22b4d3cb3a8
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-05-25 20:52:23 -0500

    updated 05/25/19 8:52pm

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,3 @@
-$("#start").on("click", function(){
-});
-
 var questions = [
   {
     question: "Q1: What is the longest river in the United States?",
@@ -29,92 +26,70 @@ var unanswered = 0;
 var timer;
 var counter = 99;
 var index = 0;
+var countdown = 0;
 
-function showQuestions() {
-var question = $("<p>");
+$("#start").on("click", function(){
+});
+  //*show trivia questions and append them
+  for (let i = 0; i < questions.length; i++) {
 
-question.append(questions[index].question + "<br>")
-question.append(questions[index].options[index])
+    $("#trivia").append("<h2>" + questions[i].question + "</h2>")
+      
+    for (let index = 0; index < questions[i].options.length; index++) {
+   
+      $("#trivia").append('<input type="radio" name="' + i + '"  res="' + index + '">' + questions[i].options[index])
+    }
+  }
+    
+  //*dynamically create/append submit button
+  $("#trivia").append('<br><br><button id="submit">Submit</button>')
+  timer = setInterval(countdown, 1000)   // if we do not have a variable, we cannot clear the interval
 
-var urlImage = $("<img>") ;
-urlImage.attr("src", questions[index].url)
+  //when submit button is clicked, check "input" selected
+  $("#submit").on("click", function () {
+    var alldata = $("#trivia").children("input:checked")
+    clearInterval(timer);
+    // stop the timer
 
-question.append(urlImage);
-  $("#trivia").append(question)
+    //store user's choices for each question in "alldata"
+    for (i = 0; i < alldata.length; i++) {
+      var q = $(alldata[i]).attr("name")
+      var r = $(alldata[i]).attr("res")
+      //split/parse user's question/correct answer to correct question/answer
+      if (parseInt(questions[q].correctAnswer) === parseInt(r)) {
+        correct++
+      }
+      else if (parseInt(questions[q].correctAnswer) !== parseInt(r)) {
+        incorrect++
+      }
+      else {
+        unanswered++
+      }
 
+      console.log("correct: ", correct);
+      console.log("incorrect: ", incorrect);
+      console.log("unanswered: ", unanswered);
+    }
 
+    //count down time left
+    function countdown() {
+      counter--;
+      $("#time-left").text("Time Remaining: " + counter)
 
+      if (counter === 0) {
+        clearInterval(timer)
+        alert("time out")
+      }
+    }
 
+    function reset() {
+      //reset your counter display here
+      //reset your counter to 0
+    }
 
-
-
-}
-
-showQuestions()
-
-
-
-//***********************SET INTERVAL 
-//***********************SET TIME OUT
-
-
-//   //*show trivia questions and append them
-//   for (let i = 0; i < questions.length; i++) {
-//     $("#trivia").append("<h2>" + questions[i].question + "</h2>")
-
-//     //*show answer choices and append them using radio buttons
-//     for (let index = 0; index < questions[i].options.length; index++) {
-//       $("#trivia").append('<input type="radio" name="' + i + '"  res="' + index + '">' + questions[i].options[index])
-//     }
-//   }
-//   //*dynamically create/append submit button
-//   $("#trivia").append('<br><br><button id="submit">Submit</button>')
-//   timer = setInterval(countdown, 1000)   // if we do not have a variable, we cannot clear the interval
-
-//   //when submit button is clicked, check "input" selected
-//   $("#submit").on("click", function () {
-//     var alldata = $("#trivia").children("input:checked")
-//     clearInterval(timer);
-//     // stop the timer
-
-//     //store user's choices for each question in "alldata"
-//     for (i = 0; i < alldata.length; i++) {
-//       var q = $(alldata[i]).attr("name")
-//       var r = $(alldata[i]).attr("res")
-//       //split/parse user's question/correct answer to correct question/answer
-//       if (parseInt(questions[q].correctAnswer) === parseInt(r)) {
-//         correct++
-//       }
-//       else if (parseInt(questions[q].correctAnswer) !== parseInt(r)) {
-//         incorrect++
-//       }
-//       else {
-//         unanswered++
-//       }
-
-//       console.log("correct: ", correct);
-//       console.log("incorrect: ", incorrect);
-//       console.log("unanswered: ", unanswered);
-//     }
-
-//     //count down time left
-//     function countdown() {
-//       counter--;
-//       $("#time-left").text("Time Remaining: " + counter)
-
-//       if (counter === 0) {
-//         clearInterval(timer)
-//         alert("time out")
-//       }
-//     }
-
-//     function reset() {
-//       //reset your counter display here
-//       //reset your counter to 0
-//     }
-
-//     reset()
-//     showQuestions()
-//   })
+    reset()
+    // showQuestions()
+  })
 
 
+// });
